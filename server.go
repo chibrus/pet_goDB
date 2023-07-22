2684a31d65f750924149601ba004495e73103117
chibrus <chibirovruslan@gmail.com>
2023-07-22 12:34:14 +0300

    new step

--- server.go
+++ server.go
@@ -1,40 +1,78 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"os"
 
 	"github.com/gofiber/fiber/v2"
+
+	"github.com/gofiber/template/html"
 )
 
+func indexHandler(c *fiber.Ctx, db *sql.DB) error {
+	var res string
+	var heyheyrusnigga []string
+	rows, err := db.Query("SELECT * FROM heyheyrunigga")
+	defer rows.Close()
+	if err != nil {
+		log.Fatalln(err)
+		c.JSON("An error occured")
+	}
+	for rows.Next() {
+		rows.Scan(&res)
+		heyheyrusnigga = append(heyheyrusnigga, res)
+	}
+	return c.Render("index", fiber.Map{
+		"heyheyrusnigga": heyheyrusnigga,
+	})
+}
+
+func postHandler(c *fiber.Ctx, db *sql.DB) error {
+	return c.SendString("Hello")
+}
+
+func putHandler(c *fiber.Ctx, db *sql.DB) error {
+	return c.SendString("Hello")
+}
+
+func deleteHandler(c *fiber.Ctx, db *sql.DB) error {
+	return c.SendString("Hello")
+}
+
 func main() {
-	app := fiber.New()
+	connStr := "postgresql://<postgres>:<1313>@<localhost>/heyheyrusnigga?sslmode=disable"
 
-	app.Get("/", indexHandler) // Add this
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	app.Post("/", postHandler) // Add this
+	engine := html.New("./views", ".html")
+	app := fiber.New(fiber.Config{
+		Views: engine,
+	})
 
-	app.Put("/update", putHandler) // Add this
+	app.Get("/", func(c *fiber.Ctx) error {
+		return indexHandler(c, db)
+	})
 
-	app.Delete("/delete", deleteHandler) // Add this
+	app.Post("/", func(c *fiber.Ctx) error {
+		return postHandler(c, db)
+	})
+
+	app.Put("/update", func(c *fiber.Ctx) error {
+		return putHandler(c, db)
+	})
+
+	app.Delete("/delete", func(c *fiber.Ctx) error {
+		return deleteHandler(c, db)
+	})
 
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "3000"
 	}
 	log.Fatalln(app.Listen(fmt.Sprintf(":%v", port)))
-}
-
-func indexHandler(c *fiber.Ctx) error {
-	return c.SendString("Hello")
-}
-func postHandler(c *fiber.Ctx) error {
-	return c.SendString("Hello")
-}
-func putHandler(c *fiber.Ctx) error {
-	return c.SendString("Hello")
-}
-func deleteHandler(c *fiber.Ctx) error {
-	return c.SendString("Hello")
 }
