010fb9765553b20d3e3351703eea9ee34a4afdbf
chibrus <chibirovruslan@gmail.com>
2023-07-23 13:46:40 +0300

    fix ip

--- server.go
+++ server.go
@@ -66,7 +66,7 @@ func deleteHandler(c *fiber.Ctx, db *sql.DB) error {
 }
 
 func main() {
-	connStr := "postgresql://<postgres>:<1313>@<localhost>/todos?sslmode=disable"
+	connStr := "postgresql://<postgres>:<1313>@<127.0.0.1:5432>/todos?sslmode=disable"
 
 	db, err := sql.Open("postgres", connStr)
 	if err != nil {
